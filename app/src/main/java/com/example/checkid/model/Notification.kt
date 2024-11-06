7714b5a8cdd820716b928e64cfacd68f3d741784
Heeflower5001 <gmltmd3tp@naver.com>
2024-11-06 22:38:08 +0900

    solve the Problem (Two fragments exist at the same time)

--- app/src/main/java/com/example/checkid/model/Notification.kt
+++ app/src/main/java/com/example/checkid/model/Notification.kt
@@ -2,9 +2,15 @@ package com.example.checkid.model
 
 class Notification (
     val notificationType : NotificationType = NotificationType.SYSTEM,
-    val textTitle : String = "",
     val textContent : String = ""
     )
+{
+    val textTitle : String = when (notificationType) {
+        NotificationType.SYSTEM -> "시스템"
+        NotificationType.REPORT -> "보고서"
+        NotificationType.WARNING -> "경고"
+    }
+}
 
 enum class NotificationType {
     SYSTEM,
--- app/src/main/java/com/example/checkid/view/MainActivity.kt
+++ app/src/main/java/com/example/checkid/view/MainActivity.kt
@@ -1,23 +1,69 @@
 package com.example.checkid.view
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.checkid.R
+import com.example.checkid.databinding.ActivityMainBinding
+import com.example.checkid.view.fragment.EmptyFragment
 import com.example.checkid.view.fragment.NotificationFragment
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            val fragment = NotificationFragment()
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, fragment)
-                .commit()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
+        supportFragmentManager.beginTransaction()
+            .setReorderingAllowed(true)
+            .replace(R.id.activity_main_FragmentContainerView, EmptyFragment())
+            .commit()
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            item -> when (item.itemId) {
+                R.id.page_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .setReorderingAllowed(true)
+                        .replace(R.id.activity_main_FragmentContainerView, EmptyFragment())
+                        .commit()
+
+                    true
+                }
+
+                R.id.page_statistics -> {
+                    supportFragmentManager.beginTransaction()
+                        .setReorderingAllowed(true)
+                        .replace(R.id.activity_main_FragmentContainerView, EmptyFragment())
+                        .commit()
+
+                    true
+                }
+
+                R.id.page_notification -> {
+                    supportFragmentManager.beginTransaction()
+                        .setReorderingAllowed(true)
+                        .replace(R.id.activity_main_FragmentContainerView, NotificationFragment())
+                        .commit()
+
+                    true
+                }
+
+                R.id.page_setting -> {
+                    supportFragmentManager.beginTransaction()
+                        .setReorderingAllowed(true)
+                        .replace(R.id.activity_main_FragmentContainerView, EmptyFragment())
+                        .commit()
+
+                    true
+                }
+
+                else -> false
+            }
         }
     }
 }
